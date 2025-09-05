b9be86b6c7c923898230441a0359f50b75d5d0cf
sarkar niloy <sarkarniloy102@gmail.com>
2025-09-05 22:17:37 +0600

    create resume function in resumecontroller.js

---
+++ controllers/resumeController.js
@@ -0,0 +1,83 @@
+import Resume from '../models/resumeModel'
+
+export const createResume = async (req, res) => {
+    try {
+        const { title } = req.body;
+
+        // default templete
+        const defaultResumeData = {
+            profileInfo: {
+                profileImg: null,
+                previewUrl: '',
+                fullName: '',
+                designation: '',
+                summary: '',
+            },
+            contactInfo: {
+                email: '',
+                phone: '',
+                location: '',
+                linkedin: '',
+                github: '',
+                website: '',
+            },
+            workExperience: [
+                {
+                    company: '',
+                    role: '',
+                    startDate: '',
+                    endDate: '',
+                    description: '',
+                },
+            ],
+            education: [
+                {
+                    degree: '',
+                    institution: '',
+                    startDate: '',
+                    endDate: '',
+                },
+            ],
+            skills: [
+                {
+                    name: '',
+                    progress: 0,
+                },
+            ],
+            projects: [
+                {
+                    title: '',
+                    description: '',
+                    github: '',
+                    liveDemo: '',
+                },
+            ],
+            certifications: [
+                {
+                    title: '',
+                    issuer: '',
+                    year: '',
+                },
+            ],
+            languages: [
+                {
+                    name: '',
+                    progress: '',
+                },
+            ],
+            interests: [''],
+        };
+
+        const newResume = await Resume.create({
+            userId: req.user._id,
+            title,
+            ...defaultResumeData,
+            ...req.body
+        })
+
+        res.status(201).json(newResume)
+
+    } catch (error) {
+res.status(500).json({message: "Failed to create resume",error: error.message})
+    }
+}
--- models/resumeModel.js
+++ models/resumeModel.js
@@ -31,6 +31,56 @@ const ResumeSchema = new mongoose.Schema({
         github: String,
         website: String
     },
+    // work experience
+    workExperience: [
+        {
+            company: String,
+            role: String,
+            startDate: String,
+            endDate: String,
+            description: String,
+        }
+    ],
+    education: [
+        {
+            degree: String,
+            institution: String,
+            startDate: String,
+            endDate: String,
+        }
+    ],
+    skills: [
+        {
+            name: String,
+            progress: Number,
+        }
+    ],
+    projects: [
+        {
+            title: String,
+            description: String,
+            github: String,
+            liveDemo: String,
+        }
+    ],
+    certifications: [
+        {
+            title: String,
+            issuer: String,
+            year: String
+        }
+    ],
+    languages: [
+        {
+            name: String,
+            progress: Number
+        }
+    ],
+    interests: [String],
 
-    
-})
+},
+    {
+        timestamps: { createdAt: "createAt", updatedAt: "updatedAt" }
+    }
+);
+export default mongoose.model("Resume", ResumeSchema);
