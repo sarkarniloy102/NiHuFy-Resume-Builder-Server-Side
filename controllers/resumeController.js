1b9507e021f1f8184835ccc8480b0db1d09912eb
sarkar niloy <sarkarniloy102@gmail.com>
2025-09-06 19:27:44 +0600

    implement get function ,upadate resume,delete resume

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -1,5 +1,6 @@
 import Resume from '../models/resumeModel'
-
+import fs from 'fs'
+import path from 'path';
 export const createResume = async (req, res) => {
     try {
         const { title } = req.body;
@@ -78,6 +79,100 @@ export const createResume = async (req, res) => {
         res.status(201).json(newResume)
 
     } catch (error) {
-res.status(500).json({message: "Failed to create resume",error: error.message})
+        res.status(500).json({ message: "Failed to create resume", error: error.message })
+    }
+}
+
+// get function
+export const getUserResumes = async (req, res) => {
+    try {
+        const resumes = await Resume.find({ userId: req.user._id }).sort({
+            updatedAt: -1
+        });
+        res.json(resumes);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get resume", error: error.message })
+    }
+}
+
+// get resume by id
+export const getResumeById = async (req, res) => {
+    try {
+        const resume = await Resume.findOne({
+            _id: req.params.id,
+            userId: req.user._id
+        })
+
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" })
+        }
+        res.json(resume)
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get resume", error: error.message })
+    }
+}
+
+// update resume function
+
+export const updateResume = async (req, res) => {
+    try {
+        const resume = await Resume.findOne({
+            _id: req.params.id,
+            userId: req.user._id
+        })
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found or not authorized" })
+        }
+        // merge updated resumes
+        Object.assign(resume, req.body)
+        // save updated resume
+        const savedResume = await resume.save();
+        res.json(savedResume);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update resume", error: error.message })
+    }
+}
+
+// delete resume
+
+export const deleteResume = async (req, res) => {
+    try {
+        const resume = await Resume.findOne({
+            _id: req.params.id,
+            userId: req.user._id
+        })
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found or not authorized" })
+        }
+        // create a uploads folder and store the resume there
+        const uploadsFolder = path.join(process.cwd(), 'uploads')
+        // delete thumbnail function
+        if (resume.thumbnailLink) {
+            const oldThumbnail = path.join(uploadsFolder, path.basename(resume.thumbnailLink))
+            if (fs.existsSync(oldThumbnail)) {
+                fs.unlinkSync(oldThumbnail)
+            }
+        }
+        if (resume.profileInfo?.profilePreviewUrl) {
+            const oldProfile = path.join(
+                uploadsFolder,
+                path.basename(resume.profileInfo.profilePreviewUrl)
+            )
+            if (fs.existsSync(oldProfile)) {
+                fs.unlinkSync(oldProfile)
+            }
+        }
+
+        // delete reume doc
+        const deleted = await Resume.findOneAndDelete({
+            _id: req.params.id,
+            userId: req.user._id
+        })
+        if (!deleted) {
+            return res.status(404).json({ message: "Resume not found or not authorized" })
+        }
+        res.json({ message: "Resume deleted successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update resume", error: error.message })
     }
 }
